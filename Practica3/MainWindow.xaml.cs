41b79610d1b072f835cf31ecf3139ef3d2674a1b
Tagir <144275124+Jynui@users.noreply.github.com>
2024-04-17 04:11:52 +0300

    Add files via upload

---
+++ Practica3/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Practica3.yandex_storeDataSetTableAdapters;
+namespace Practica3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OrdersTableAdapter ordersTableAdapter = new OrdersTableAdapter();
+                                                            
+
+        public MainWindow()
+        {
+            InitializeComponent();
+             datagrid.ItemsSource = ordersTableAdapter.GetDataBy();
+
+        }
+    }
+}
+
---
+++ SQLQuery1.sql
@@ -0,0 +1,26 @@
+﻿INSERT INTO Orders ( order_date, total_amount) VALUES
+('2024-04-10', 150.00),
+('2024-04-11', 200.00),
+('2024-04-11', 120.00);
+
+-- Добавление данных в таблицу Товары
+INSERT INTO Products ( product_name, price, stock_quantity) VALUES
+( 'Футболка', 25.00, 50),
+( 'Джинсы', 40.00, 30),
+( 'Кроссовки', 60.00, 20),
+( 'Рубашка', 30.00, 40);
+
+-- Добавление данных в таблицу Доставка
+INSERT INTO Delivery ( orders_id, delivery_status, estimated_delivery_date) VALUES
+( 1, 'в пути', '2024-04-12'),
+( 1, 'в обработке', '2024-04-11'),
+( 1, 'доставлено', '2024-04-13');
+
+-- Добавление данных в промежуточную таблицу Order_Products
+
+INSERT INTO Order_Products (order_id,products_id, quantity) VALUES
+(1, 1, 2),
+(1, 1, 1),
+(1, 1, 3),
+(1, 1, 2),
+(1, 1, 1);
